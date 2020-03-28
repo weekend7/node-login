cef6e7a86b45b53605d84a66b40f37efa744d059
weekend7 <39322384+weekend7@users.noreply.github.com>
2020-03-28 15:26:07 +0800

    修复注册失败

--- app.js
+++ app.js
@@ -3,81 +3,83 @@ const url = require('url')
 const querystring = require('querystring') // 该模块用于解析和格式化 URL 查询字符串
 const fs = require('fs')
 
-let user={
+let user = {
   admin: 123456
 };
 
-http.createServer((req,res) =>{
+http.createServer((req, res) => {
   // 获取数据
-  let path,get,post
-  if(req.method=='GET'){
-    let {pathname,query} = url.parse(req.url,true)
-    path=pathname,
-    get=query
+  let path, get, post
+  if (req.method == 'GET') {
+    let { pathname, query } = url.parse(req.url, true)
+    path = pathname,
+      get = query
     complete()
-  }else if(req.method == 'POST'){
-    let arr=[]
-    path=req.url
-    req.on('data',buffer =>{
-      arr.push(buffer) 
+  } else if (req.method == 'POST') {
+    let arr = []
+    path = req.url
+    req.on('data', buffer => {
+      arr.push(buffer)
     })
-    req.on('end',()=>{
-      post = querystring.parse(Buffer.concat(arr).toString()) 
+    req.on('end', () => {
+      post = querystring.parse(Buffer.concat(arr).toString())
       complete()
     })
-    
+
   }
 
   function complete() {
-    if(path == '/login'){
-      let {username,password}=get
 
-      if(!user[username]){
+    if (path == '/login') {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain;charset=utf-8'
+      })
+      let { username, password } = get;
+      if (!user[username]) {
         res.end(JSON.stringify({
-          err:1,
-          msg:"用户名不存在"
+          err: 1,
+          msg: '用户名不存在'
+        }))
+      } else if (user[username] != password) {
+        res.writeHead(200, {
+          'Content-Type': 'text/plain;charset=utf-8'
+        })
+        res.end(JSON.stringify({
+          err: 1,
+          msg: '密码不正确'
         })
         )
-      }else if(user[username]!=password) {
+      } else {
         res.end(JSON.stringify({
-          err:1,
-          msg:"密码错误"
-        })
-        )
-      }else {
-        res.end(JSON.stringify({
-          err:0,
-          msg:"登陆成功"
-        })
-        )
+          err: 0,
+          msg: '登陆成功'
+        }))
       }
-
-    }else if(path == '/reg'){
-      let {username,password}=post
-
-      if(user[username]){
+    } else if (path == '/reg') {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain;charset=utf-8'
+      });
+      let { username, password } = post;
+      if (user[username]) {
         res.end(JSON.stringify({
-          err:1,
-          msg:"账户已存在"
-        })
-        )
-      }else{
-        user[username]=password
+          err: 1,
+          msg: '用户已经存在'
+        }))
+      } else {
+        user[username] = password;
         res.end(JSON.stringify({
-          err:0,
-          msg:"注册成功"
-        })
-        )
+          err: 0,
+          msg: '注册成功'
+        }))
       }
-    }else{
-      fs.readFile(`www${path}`,(err,data)=>{
-        if(err) {
-          res.end('404')
-        }else{
-          res.end(data)
+    } else {
+      fs.readFile(`www${path}`, (err, data) => {
+        if (err) {
+          res.end('404');
+        } else {
+          res.end(data);
         }
       })
     }
   }
-
 }).listen(8080)
